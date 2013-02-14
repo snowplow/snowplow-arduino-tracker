11d38bfe488f8a6aad3bf5ec2e692b207815ccf2
Alexander Dean <alex@snowplowanalytics.com>
2013-02-14 00:42:18 +0000

    Update SnowPlowTracker.cpp

--- SnowPlowTracker.cpp
+++ SnowPlowTracker.cpp
@@ -410,6 +410,7 @@ char *SnowPlowTracker::urlEncode(const char* aMsg)
       encodedMsg[idx++] = hex[*aMsg >> 4];
       encodedMsg[idx++] = hex[*aMsg & 15];
     }
+    *aMsg++;
   }
   encodedMsg[idx] = '\0'; // tail it
   return encodedMsg;
